f715c5d2f6436b71d7df5c323d8dae5d2cf963e2
Sakil M <sakilmahmudmolla@gmail.com>
2025-01-09 10:42:01 +0530

    minor update in dtp

--- application/views/admin/dtp/add.php
+++ application/views/admin/dtp/add.php
@@ -24,82 +24,98 @@
                             }
                             ?>
                             <form action="<?php echo $action; ?>" method="post">
-                                <div class="form-group">
-                                    <label for="service_descriptions">Service Description</label>
-                                    <input type="text" class="form-control" id="service_descriptions" name="service_descriptions" value="<?php echo set_value('service_descriptions', isset($service['service_descriptions']) ? $service['service_descriptions'] : ''); ?>" required>
-                                    <?php echo form_error('service_descriptions'); ?>
-                                </div>
-                                <div class="form-group">
-                                    <label for="dtp_service_categories">Category</label>
-                                    <select class="form-control" id="dtp_service_categories" name="dtp_service_categories">
-                                        <option value="">Select Category</option>
-                                        <?php foreach ($categories as $category) : ?>
-                                            <option value="<?php echo $category['id']; ?>"
-                                                <?php
-                                                echo set_select(
-                                                    'dtp_service_categories',
-                                                    $category['id'],
-                                                    isset($service['dtp_service_category_id'])
-                                                        ? $service['dtp_service_category_id'] == $category['id']
-                                                        : $category['id'] == 1 // Default to category with ID 1
-                                                );
-                                                ?>>
-                                                <?php echo $category['cat_title']; ?>
-                                            </option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                    <?php echo form_error('dtp_service_categories'); ?>
-                                </div>
-                                <div class="form-group">
-                                    <label for="service_charge">Service Charge</label>
-                                    <input type="number" class="form-control" id="service_charge" name="service_charge" value="<?php echo set_value('service_charge', isset($service['service_charge']) ? $service['service_charge'] : ''); ?>" required>
-                                    <?php echo form_error('service_charge'); ?>
-                                </div>
-                                <div class="form-group">
-                                    <label for="paid_status">Paid Status</label><br>
-                                    <label>
-                                        <input type="radio" name="paid_status" value="1"
-                                            <?php echo set_radio('paid_status', '1', isset($service['paid_status'])
-                                                ? $service['paid_status'] == 1
-                                                : true); // Default to "Full Paid"
-                                            ?>> Full Paid
-                                    </label>
-                                    <label>
-                                        <input type="radio" name="paid_status" value="2"
-                                            <?php echo set_radio('paid_status', '2', isset($service['paid_status'])
-                                                ? $service['paid_status'] == 2
-                                                : false);
-                                            ?>> Partial
-                                    </label>
-                                    <label>
-                                        <input type="radio" name="paid_status" value="0"
-                                            <?php echo set_radio('paid_status', '0', isset($service['paid_status'])
-                                                ? $service['paid_status'] == 0
-                                                : false);
-                                            ?>> Due
-                                    </label>
-                                    <?php echo form_error('paid_status'); ?>
-                                </div>
-                                <!-- Hidden input for Partial Paid -->
-                                <div class="form-group" id="partial_paid_container" style="display: none;">
-                                    <label for="partial_paid">Partial Paid Amount</label>
-                                    <input type="number" class="form-control" id="partial_paid" name="partial_paid" value="<?php echo set_value('partial_paid', isset($service['partial_paid']) ? $service['partial_paid'] : ''); ?>">
-                                    <?php echo form_error('partial_paid'); ?>
-                                </div>
-                                <div class="form-group">
-                                    <label for="service_date">Service Date</label>
-                                    <input type="date" class="form-control" id="service_date" name="service_date" value="<?php echo set_value('service_date', isset($service['service_date']) ? $service['service_date'] : date('Y-m-d')); ?>">
-                                    <?php echo form_error('service_date'); ?>
-                                </div>
-                                <!-- New Payment Mode Field -->
-                                <div class="form-group payment_method_section">
-                                    <select class="form-control payment_method_id" name="payment_mode">
-                                        <?php if (!empty($payment_methods)) :
-                                            foreach ($payment_methods as $index => $paymentMode) :
-                                                echo '<option value="' . $paymentMode['id'] . '">' . $paymentMode['title'] . '</option>';
-                                            endforeach;
-                                        endif; ?>
-                                    </select>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="service_descriptions">Service Description</label>
+                                            <input type="text" class="form-control" id="service_descriptions" name="service_descriptions" value="<?php echo set_value('service_descriptions', isset($service['service_descriptions']) ? $service['service_descriptions'] : ''); ?>" required>
+                                            <?php echo form_error('service_descriptions'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="dtp_service_categories">Category</label>
+                                            <select class="form-control" id="dtp_service_categories" name="dtp_service_categories">
+                                                <option value="">Select Category</option>
+                                                <?php foreach ($categories as $category) : ?>
+                                                    <option value="<?php echo $category['id']; ?>"
+                                                        <?php
+                                                        echo set_select(
+                                                            'dtp_service_categories',
+                                                            $category['id'],
+                                                            isset($service['dtp_service_category_id'])
+                                                                ? $service['dtp_service_category_id'] == $category['id']
+                                                                : $category['id'] == 1 // Default to category with ID 1
+                                                        );
+                                                        ?>>
+                                                        <?php echo $category['cat_title']; ?>
+                                                    </option>
+                                                <?php endforeach; ?>
+                                            </select>
+                                            <?php echo form_error('dtp_service_categories'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="service_charge">Service Charge</label>
+                                            <input type="number" class="form-control" id="service_charge" name="service_charge" value="<?php echo set_value('service_charge', isset($service['service_charge']) ? $service['service_charge'] : ''); ?>" required>
+                                            <?php echo form_error('service_charge'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <!-- New Payment Mode Field -->
+                                        <div class="form-group payment_method_section">
+                                            <label for="service_charge">Payment Mode</label>
+                                            <select class="form-control payment_method_id" name="payment_mode">
+                                                <?php if (!empty($payment_methods)) :
+                                                    foreach ($payment_methods as $index => $paymentMode) :
+                                                        echo '<option value="' . $paymentMode['id'] . '">' . $paymentMode['title'] . '</option>';
+                                                    endforeach;
+                                                endif; ?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="paid_status">Paid Status</label><br>
+                                            <label>
+                                                <input type="radio" name="paid_status" value="1"
+                                                    <?php echo set_radio('paid_status', '1', isset($service['paid_status'])
+                                                        ? $service['paid_status'] == 1
+                                                        : true); // Default to "Full Paid"
+                                                    ?>> Full Paid
+                                            </label>
+                                            <label>
+                                                <input type="radio" name="paid_status" value="2"
+                                                    <?php echo set_radio('paid_status', '2', isset($service['paid_status'])
+                                                        ? $service['paid_status'] == 2
+                                                        : false);
+                                                    ?>> Partial
+                                            </label>
+                                            <label>
+                                                <input type="radio" name="paid_status" value="0"
+                                                    <?php echo set_radio('paid_status', '0', isset($service['paid_status'])
+                                                        ? $service['paid_status'] == 0
+                                                        : false);
+                                                    ?>> Due
+                                            </label>
+                                            <?php echo form_error('paid_status'); ?>
+                                        </div>
+                                        <!-- Hidden input for Partial Paid -->
+                                        <div class="form-group" id="partial_paid_container" style="display: none;">
+                                            <label for="partial_paid">Partial Paid Amount</label>
+                                            <input type="number" class="form-control" id="partial_paid" name="partial_paid" value="<?php echo set_value('partial_paid', isset($service['paid_amount']) ? $service['paid_amount'] : ''); ?>">
+                                            <?php echo form_error('partial_paid'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+
+                                        <div class="form-group">
+                                            <label for="service_date">Service Date</label>
+                                            <input type="date" class="form-control" id="service_date" name="service_date" value="<?php echo set_value('service_date', isset($service['service_date']) ? $service['service_date'] : date('Y-m-d')); ?>">
+                                            <?php echo form_error('service_date'); ?>
+                                        </div>
+                                    </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary"><?php echo ($isUpdate) ? "Update" : "Add"; ?></button>
                             </form>
@@ -122,7 +138,7 @@
                 $('#partial_paid').prop('required', true);
             } else {
                 $('#partial_paid_container').hide();
-                $('#partial_paid').prop('required', false).val('');
+                $('#partial_paid').prop('required', false);
             }
         });
 
