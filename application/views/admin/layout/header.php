df2dc35cb231f66d2cccc715b100a9d0222ea999
Sakil M <sakilmahmudmolla@gmail.com>
2025-06-26 00:36:22 +0530

    remove unnessory files

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -92,6 +92,10 @@
 <div class="modal fade" id="calculatorModal" tabindex="-1" aria-labelledby="calculatorModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="taskModalLabel">Calculator</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
             <div class="modal-body p-0">
                 <div class="calculator">
                     <div class="display">
@@ -124,9 +128,6 @@
                         <button class="equals">=</button>
                     </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
